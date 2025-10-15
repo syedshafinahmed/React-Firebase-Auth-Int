f6c15c1747553b1db3299b78f0445a3364ed8133
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-16 01:37:15 +0600

    added observer

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Context/AuthContext/AuthContext';
 
 const Login = () => {
+
+  const { signInUser } = use(AuthContext);
+  console.log('login');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    signInUser(email, password)
+      .then(result => {
+        console.log(result.user);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
   return (
     <div className='mt-20'>
       <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
         <div className="card-body">
           <h1 className="text-3xl font-bold text-center">Login now!</h1>
-          <form>
+          <form onSubmit={handleLogin}>
             <fieldset className="fieldset">
               {/* email  */}
               <label className="label">Email</label>
--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   }
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      console.log("inside observer", currentUser);
+    }
+    else {
+      console.log("else observer", currentUser);
+    }
+  })
   const authInfo = {
     // createUser: createUser
     createUser,
+    signInUser
   }
   return (
     <AuthContext value={authInfo}>
